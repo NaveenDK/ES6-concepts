cd5af0c34f3a7038191a97a661203fb65c63d834
naveendk <naveenkolambage@gmail.com>
2019-06-24 07:11:45 +1200

    second commit

--- all.js
+++ all.js
@@ -15,3 +15,30 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 for (let i = 0; i < companies.length; i++) {
   console.log(companies[i]);
 }
+
+//print all the companies as individual objects
+
+//a) use for loop
+//b) use forEach
+//c) use filter
+//
+
+//print all the ages 21 and over
+
+//a)use for loop
+//b) use filter higher order function
+//c) use ES6 arrow functions
+let canDrink = [];
+for (let i = 0; i < ages.length; i++) {
+  if (ages[i] >= 21) {
+    canDrink.push(ages[i]);
+  }
+}
+
+// const canDrink = ages.filter(function(age) {
+//   if (age >= 21) {
+//     return true;
+//   }
+// });
+
+console.log(canDrink);
